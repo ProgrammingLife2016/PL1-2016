4c16a24d845e64bf844abc4591f484f08a29ac1e
Ali Smesseim <smesseim.ali@gmail.com>
2016-04-19 13:27:03 +0200

    Fix checkstyle warnings

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,4 +1,14 @@
-public class HelloWorld {
+/**
+ * 'Utility' class to test the workings of the project.
+ */
+public final class HelloWorld {
+    private HelloWorld() {
+    }
+    /**
+     * Print "Hello, world!".
+     *
+     * @param args ignored command line arguments.
+     */
     public static void main(String[] args) {
         System.out.println("Hello, world!");
     }
--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -2,7 +2,13 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
 
+/**
+ * Simple test class to test if JUnit works.
+ */
 public class SimpleTest {
+    /**
+     * Assert that true == true.
+     */
     @Test
     public void test() {
         assertTrue(true);
