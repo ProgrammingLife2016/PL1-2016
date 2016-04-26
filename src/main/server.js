a8ea0678c8505ebb90eea798e22d00b7030d6d68
SamuelSital <samuelsital@gmail.com>
2016-04-27 01:50:33 +0200

    Changed structure main.js

--- src/main/server.js
+++ src/main/server.js
@@ -1,19 +1,7 @@
 var http = require("http");
 var express = require("express");
 var url = require("url");
-var mysql = require("mysql");
 var ejs = require("ejs");
-
-//Initialize database connection
-var database = mysql.createConnection({
-	host: 'localhost',
-	user: 'root',
-	password: '',
-	database: 'todo'
-});
-
-//Connect to database
-database.connect();
 
 //Initialize server
 var app = express();
@@ -21,27 +9,8 @@ var app = express();
 app.get("/nodes", function(req, res) {
     res.json({"nodes": [{ "data": {"id": '1', "name": 'A', "selected": true}}, {"data": {"id": '2', "name": 'G'}}],
               "edges": [{ "data": { "source": '1', "target": '2', "group": 'pi' } }]});
-//      res.json("{'nodes': [{'classes': 'b', 'data': {'id': 'n0', 'weight': 40}}, {'classes': 'b', 'data': {'id': 'n1', 'weight': 19}}, {'classes': 'c', 'data': {'id': 'n2', 'weight': 0}}, {'classes': 'd', 'data': {'id': 'n3', 'weight': 19}}, {'classes': 'e', 'data': {'id': 'n4', 'weight': 4}}, {'classes': 'a', 'data': {'id': 'n5', 'weight': 21}}, {'classes': 'e', 'data': {'id': 'n6', 'weight': 13}}, {'classes': 'a', 'data': {'id': 'n7', 'weight': 6}}, {'classes': 'a', 'data': {'id': 'n8', 'weight': 10}}, {'classes': 'b', 'data': {'id': 'n9', 'weight': 6}}, {'classes': 'c', 'data': {'id': 'n10', 'weight': 24}}, {'classes': 'c', 'data': {'id': 'n11', 'weight': 14}}, {'classes': 'e', 'data': {'id': 'n12', 'weight': 11}}, {'classes': 'b', 'data': {'id': 'n13', 'weight': 6}}, {'classes': 'e', 'data': {'id': 'n14', 'weight': 24}}, {'classes': 'b', 'data': {'id': 'n15', 'weight': 26}}, {'classes': 'b', 'data': {'id': 'n16', 'weight': 6}}, {'classes': 'c', 'data': {'id': 'n17', 'weight': 36}}, {'classes': 'a', 'data': {'id': 'n18', 'weight': 7}}, {'classes': 'b', 'data': {'id': 'n19', 'weight': 37}}, {'classes': 'c', 'data': {'id': 'n20', 'weight': 28}}, {'classes': 'd', 'data': {'id': 'n21', 'weight': 11}}, {'classes': 'd', 'data': {'id': 'n22', 'weight': 20}}, {'classes': 'e', 'data': {'id': 'n23', 'weight': 7}}, {'classes': 'b', 'data': {'id': 'n24', 'weight': 31}}, {'classes': 'b', 'data': {'id': 'n25', 'weight': 1}}, {'classes': 'e', 'data': {'id': 'n26', 'weight': 29}}, {'classes': 'd', 'data': {'id': 'n27', 'weight': 31}}, {'classes': 'd', 'data': {'id': 'n28', 'weight': 34}}, {'classes': 'b', 'data': {'id': 'n29', 'weight': 40}}, {'classes': 'd', 'data': {'id': 'n30', 'weight': 38}}, {'classes': 'b', 'data': {'id': 'n31', 'weight': 17}}, {'classes': 'a', 'data': {'id': 'n32', 'weight': 39}}, {'classes': 'd', 'data': {'id': 'n33', 'weight': 4}}, {'classes': 'c', 'data': {'id': 'n34', 'weight': 38}}, {'classes': 'd', 'data': {'id': 'n35', 'weight': 13}}, {'classes': 'b', 'data': {'id': 'n36', 'weight': 15}}, {'classes': 'a', 'data': {'id': 'n37', 'weight': 29}}, {'classes': 'a', 'data': {'id': 'n38', 'weight': 2}}, {'classes': 'd', 'data': {'id': 'n39', 'weight': 35}}, {'classes': 'c', 'data': {'id': 'n40', 'weight': 24}}, {'classes': 'c', 'data': {'id': 'n41', 'weight': 7}}, {'classes': 'e', 'data': {'id': 'n42', 'weight': 24}}, {'classes': 'c', 'data': {'id': 'n43', 'weight': 4}}, {'classes': 'd', 'data': {'id': 'n44', 'weight': 40}}, {'classes': 'a', 'data': {'id': 'n45', 'weight': 19}}, {'classes': 'b', 'data': {'id': 'n46', 'weight': 17}}, {'classes': 'b', 'data': {'id': 'n47', 'weight': 36}}, {'classes': 'b', 'data': {'id': 'n48', 'weight': 26}}, {'classes': 'a', 'data': {'id': 'n49', 'weight': 18}}], 'edges': [{'data': {'source': 'n5', 'id': 'e0', 'weight': 31, 'target': 'n19'}}, {'data': {'source': 'n37', 'id': 'e1', 'weight': 31, 'target': 'n25'}}, {'data': {'source': 'n19', 'id': 'e2', 'weight': 31, 'target': 'n2'}}, {'data': {'source': 'n16', 'id': 'e3', 'weight': 23, 'target': 'n27'}}, {'data': {'source': 'n29', 'id': 'e4', 'weight': 17, 'target': 'n4'}}, {'data': {'source': 'n1', 'id': 'e5', 'weight': 33, 'target': 'n12'}}, {'data': {'source': 'n13', 'id': 'e6', 'weight': 38, 'target': 'n33'}}, {'data': {'source': 'n12', 'id': 'e7', 'weight': 34, 'target': 'n4'}}, {'data': {'source': 'n32', 'id': 'e8', 'weight': 34, 'target': 'n13'}}, {'data': {'source': 'n44', 'id': 'e9', 'weight': 32, 'target': 'n19'}}, {'data': {'source': 'n31', 'id': 'e10', 'weight': 24, 'target': 'n19'}}, {'data': {'source': 'n35', 'id': 'e11', 'weight': 18, 'target': 'n48'}}, {'data': {'source': 'n25', 'id': 'e12', 'weight': 19, 'target': 'n15'}}, {'data': {'source': 'n31', 'id': 'e13', 'weight': 18, 'target': 'n16'}}, {'data': {'source': 'n24', 'id': 'e14', 'weight': 39, 'target': 'n27'}}, {'data': {'source': 'n47', 'id': 'e15', 'weight': 22, 'target': 'n3'}}, {'data': {'source': 'n1', 'id': 'e16', 'weight': 34, 'target': 'n35'}}, {'data': {'source': 'n22', 'id': 'e17', 'weight': 15, 'target': 'n5'}}, {'data': {'source': 'n37', 'id': 'e18', 'weight': 40, 'target': 'n10'}}, {'data': {'source': 'n37', 'id': 'e19', 'weight': 21, 'target': 'n29'}}]}
 });
 
-//  elements: {
-//    nodes: [
-//      { data: { id: '1', name: 'A', "score":0.006769776522008331 } },
-//      { data: { id: '2', name: 'T', "score": 0.006769776522008331 } },
-//      { data: { id: '3', name: 'TCGG', "score": 0.006769776522008331 } },
-//      { data: { id: '4', name: 'AAC', "score": 0.006769776522008331 } },
-//      { data: { id: '5', name: 'GTT', "score": 0.006769776522008331 } },
-//      { data: { id: '6', name: 'TAGTC', "score": 0.006769776522008331 } }
-//    ],
-//    edges: [
-//      { data: { source: '1', target: '3', group: "pi" } },
-//      { data: { source: '2', target: '3', group: "pi" } },
-//      { data: { source: '3', target: '4', group: "predict" } },
-//      { data: { source: '3', target: '5', group: "reg" } },
-//      { data: { source: '4', target: '6', group: "spd" } },
-//      { data: { source: '5', target: '6', group: "coloc" } }
-//    ]
-//  }
 app.use(express.static(__dirname + "/webapp"));
 
 http.createServer(app).listen(8000);
--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,9 +1,170 @@
 $(function(){ // on dom ready
 
-var cy = cytoscape({
-  container: $('#cy')[0],
-  boxSelectionEnabled: false,
-  autounselectify: true,
+  var cy = cytoscape({
+    container: $('#cy')[0],
+    boxSelectionEnabled: false,
+    autounselectify: true,
+
+    style: [{"selector":"core",
+               "style":
+               {"selection-box-color":"#AAD8FF","selection-box-border-color":"#8BB0D0","selection-box-opacity":"0.5"}},
+            {"selector":"node",
+               "style":
+                {"width":"mapData(score, 0, 0.006769776522008331, 20, 60)",
+                 "height":"mapData(score, 0, 0.006769776522008331, 20, 60)",
+                 "content":"data(name)","font-size":"12px","text-valign":"center","text-halign":"center",
+                 "background-color":"#555","text-outline-color":"#555","text-outline-width":"2px","color":"#fff",
+                 "overlay-padding":"6px","z-index":"10"}},
+            {"selector":"node[?attr]","style":{"shape":"rectangle","background-color":"#aaa","text-outline-color":"#aaa",
+                "width":"16px","height":"16px","font-size":"6px","z-index":"1"}},
+            {"selector":"node[?query]","style":{"background-clip":"none","background-fit":"contain"}},
+            {"selector":"node:selected","style":{"border-width":"6px","border-color":"#AAD8FF","border-opacity":"0.5",
+                "background-color":"#77828C","text-outline-color":"#77828C"}},
+            {"selector":"edge","style":{"curve-style":"haystack","haystack-radius":"0.5","opacity":"0.8",
+                "line-color":"#bbb","width":"mapData(weight, 0, 1, 1, 8)","overlay-padding":"3px"}},
+            {"selector":"node.unhighlighted","style":{"opacity":"0.2"}},
+            {"selector":"edge.unhighlighted","style":{"opacity":"0.05"}},
+            {"selector":".highlighted","style":{"z-index":"999999"}},
+            {"selector":"node.highlighted","style":{"border-width":"6px","border-color":"#AAD8FF","border-opacity":"0.5",
+                "background-color":"#394855","text-outline-color":"#394855",
+                "shadow-blur":"12px","shadow-color":"#000","shadow-opacity":"0.8",
+                "shadow-offset-x":"0px","shadow-offset-y":"4px"}},
+            {"selector": "node", "style": {"color": "#ECF0F1"}},
+            {"selector":"edge.filtered","style":{"opacity":"0"}},
+            {"selector":"edge[group=\"coexp\"]","style":{"line-color":"#d0b7d5"}},
+            {"selector":"edge[group=\"coloc\"]","style":{"line-color":"#a0b3dc"}},
+            {"selector":"edge[group=\"gi\"]","style":{"line-color":"#90e190"}},
+            {"selector":"edge[group=\"path\"]","style":{"line-color":"#9bd8de"}},
+            {"selector":"edge[group=\"pi\"]","style":{"line-color":"#eaa2a2"}},
+            {"selector":"edge[group=\"predict\"]","style":{"line-color":"#f6c384"}},
+            {"selector":"edge[group=\"spd\"]","style":{"line-color":"#dad4a2"}},
+            {"selector":"edge[group=\"spd_attr\"]","style":{"line-color":"#D0D0D0"}},
+            {"selector":"edge[group=\"reg\"]","style":{"line-color":"#D0D0D0"}},
+            {"selector":"edge[group=\"reg_attr\"]","style":{"line-color":"#D0D0D0"}},
+            {"selector":"edge[group=\"user\"]","style":{"line-color":"#f0ec86"}}],
+
+    elements: {
+      nodes: [
+        { data: { id: '1', name: 'A', "score":0.006769776522008331 } },
+        { data: { id: '2', name: 'T', "score": 0.006769776522008331 } },
+        { data: { id: '3', name: 'TCGG', "score": 0.006769776522008331 } },
+        { data: { id: '4', name: 'AAC', "score": 0.006769776522008331 } },
+        { data: { id: '5', name: 'GTT', "score": 0.006769776522008331 } },
+        { data: { id: '6', name: 'TAGTC', "score": 0.006769776522008331 } }
+      ],
+      edges: [
+        { data: { source: '1', target: '3', group: "pi" } },
+        { data: { source: '2', target: '3', group: "pi" } },
+        { data: { source: '3', target: '4', group: "predict" } },
+        { data: { source: '3', target: '5', group: "reg" } },
+        { data: { source: '4', target: '6', group: "spd" } },
+        { data: { source: '5', target: '6', group: "coloc" } }
+      ]
+    },
+
+    layout: {
+      name: 'grid',
+      padding: 10
+    }
+  });
+
+  $('#cy').cytoscapeNavigator(); // Initialize mini map
+  var serverConnection = new ServerConnection();
+  var graphHandler = new GraphHandler();
+
+  $("#connect").click(function(){
+    console.log("Connecting to server...");
+    serverConnection.retrieveDataFromServer();
+  });
+
+
+  /*
+      Adapter for converting JSON data from server to correct format for cytoscape
+  */
+  function JSONAdapter() {
+  }
+
+  /*
+    Convert JSON data received from server.
+  */
+  JSONAdapter.prototype.convert = function(data) {
+  }
+
+
+  /*
+      Class for connection with the server
+  */
+  function ServerConnection() {
+     nodesDir = "/nodes";
+  }
+
+  /*
+    Create AJAX to server
+  */
+  ServerConnection.prototype.retrieveDataFromServer = function() {
+        $.ajax({
+           url: "/nodes",
+           dataType: 'json',
+           success : this.handleSucces,
+           error: this.handleError,
+           statusCode: {
+             default: this.handleStatusCode
+           }
+        });
+  }
+
+  /*
+    Load data if AJAX request was succesfull.
+  */
+  ServerConnection.prototype.handleSucces = function(data, textStatus, jqXHR) {
+        console.log(textStatus);
+        console.log(jqXHR);
+        graphHandler.loadDataInGraph(data);
+  }
+
+  /*
+    Handle error if AJAX request failed.
+  */
+  ServerConnection.prototype.handleError = function(jqXHR ,textStatus, errorThrown) {
+        console.log("ERROR AJAX request");
+        console.log(jqXHR);
+        console.log("- HTTP status code : " +  jqXHR.status);
+        console.log("- response Text    : " + jqXHR.responseText);
+        console.log("- Error thrown     : " + errorThrown);
+  }
+
+  /*
+    Print HTTP status code of AJAX request.
+  */
+  ServerConnection.prototype.handleStatusCode = function(statusCode) {
+        console.log("Status code AJAX request: " + statusCode)
+  }
+
+
+  /*
+     Class for communicating with cytoscape
+  */
+  function GraphHandler() {
+
+  }
+
+  /*
+    Load data of graph containing nodes and edges in graph view.
+  */
+  GraphHandler.prototype.loadDataInGraph = function(elements) {
+      console.log(elements);
+      window.elements = elements;
+      cy.load(elements);
+  }
+}); // on dom ready
+
+//cy.on('tap', 'node', function(){
+//  try { // your browser may block popups
+//    window.open( this.data('href') );
+//  } catch(e){ // fall back on url change
+//    window.location.href = this.data('href');
+//  }
+//});
 
 //  style: cytoscape.stylesheet()
 //    .selector('node')
@@ -23,116 +184,3 @@ var cy = cytoscape({
 //        'text-outline-color': 'black'
 //      }),
 
-  style: [{"selector":"core",
-             "style":
-             {"selection-box-color":"#AAD8FF","selection-box-border-color":"#8BB0D0","selection-box-opacity":"0.5"}},
-          {"selector":"node",
-             "style":
-              {"width":"mapData(score, 0, 0.006769776522008331, 20, 60)",
-               "height":"mapData(score, 0, 0.006769776522008331, 20, 60)",
-               "content":"data(name)","font-size":"12px","text-valign":"center","text-halign":"center",
-               "background-color":"#555","text-outline-color":"#555","text-outline-width":"2px","color":"#fff",
-               "overlay-padding":"6px","z-index":"10"}},
-          {"selector":"node[?attr]","style":{"shape":"rectangle","background-color":"#aaa","text-outline-color":"#aaa",
-              "width":"16px","height":"16px","font-size":"6px","z-index":"1"}},
-          {"selector":"node[?query]","style":{"background-clip":"none","background-fit":"contain"}},
-          {"selector":"node:selected","style":{"border-width":"6px","border-color":"#AAD8FF","border-opacity":"0.5",
-              "background-color":"#77828C","text-outline-color":"#77828C"}},
-          {"selector":"edge","style":{"curve-style":"haystack","haystack-radius":"0.5","opacity":"0.8",
-              "line-color":"#bbb","width":"mapData(weight, 0, 1, 1, 8)","overlay-padding":"3px"}},
-          {"selector":"node.unhighlighted","style":{"opacity":"0.2"}},
-          {"selector":"edge.unhighlighted","style":{"opacity":"0.05"}},
-          {"selector":".highlighted","style":{"z-index":"999999"}},
-          {"selector":"node.highlighted","style":{"border-width":"6px","border-color":"#AAD8FF","border-opacity":"0.5",
-              "background-color":"#394855","text-outline-color":"#394855",
-              "shadow-blur":"12px","shadow-color":"#000","shadow-opacity":"0.8",
-              "shadow-offset-x":"0px","shadow-offset-y":"4px"}},
-          {"selector": "node", "style": {"color": "#ECF0F1"}},
-          {"selector":"edge.filtered","style":{"opacity":"0"}},
-          {"selector":"edge[group=\"coexp\"]","style":{"line-color":"#d0b7d5"}},
-          {"selector":"edge[group=\"coloc\"]","style":{"line-color":"#a0b3dc"}},
-          {"selector":"edge[group=\"gi\"]","style":{"line-color":"#90e190"}},
-          {"selector":"edge[group=\"path\"]","style":{"line-color":"#9bd8de"}},
-          {"selector":"edge[group=\"pi\"]","style":{"line-color":"#eaa2a2"}},
-          {"selector":"edge[group=\"predict\"]","style":{"line-color":"#f6c384"}},
-          {"selector":"edge[group=\"spd\"]","style":{"line-color":"#dad4a2"}},
-          {"selector":"edge[group=\"spd_attr\"]","style":{"line-color":"#D0D0D0"}},
-          {"selector":"edge[group=\"reg\"]","style":{"line-color":"#D0D0D0"}},
-          {"selector":"edge[group=\"reg_attr\"]","style":{"line-color":"#D0D0D0"}},
-          {"selector":"edge[group=\"user\"]","style":{"line-color":"#f0ec86"}}],
-
-  elements: {
-    nodes: [
-      { data: { id: '1', name: 'A', "score":0.006769776522008331 } },
-      { data: { id: '2', name: 'T', "score": 0.006769776522008331 } },
-      { data: { id: '3', name: 'TCGG', "score": 0.006769776522008331 } },
-      { data: { id: '4', name: 'AAC', "score": 0.006769776522008331 } },
-      { data: { id: '5', name: 'GTT', "score": 0.006769776522008331 } },
-      { data: { id: '6', name: 'TAGTC', "score": 0.006769776522008331 } }
-    ],
-    edges: [
-      { data: { source: '1', target: '3', group: "pi" } },
-      { data: { source: '2', target: '3', group: "pi" } },
-      { data: { source: '3', target: '4', group: "predict" } },
-      { data: { source: '3', target: '5', group: "reg" } },
-      { data: { source: '4', target: '6', group: "spd" } },
-      { data: { source: '5', target: '6', group: "coloc" } }
-    ]
-  },
-
-  layout: {
-    name: 'grid',
-    padding: 10
-  }
-});
-
-// Initialize mini map
-$('#cy').cytoscapeNavigator();
-var serverConnection = new ServerConnection();
-
-//cy.on('tap', 'node', function(){
-//  try { // your browser may block popups
-//    window.open( this.data('href') );
-//  } catch(e){ // fall back on url change
-//    window.location.href = this.data('href');
-//  }
-//});
-
-  $("#connect").click(function(){
-    console.log("Connecting to server...");
-    serverConnection.getDataFromServer();
-  });
-
-  /*
-      Adapter for converting JSON data from server to correct format for cytoscape
-  */
-  function JSONAdapter() {
-      this.load(data) = function(){}
-      this.convert() = function(){}
-  }
-
-  /*
-      Class for connection with the server
-  */
-  function ServerConnection() {
-
-     this.getDataFromServer = function() {
-        $.ajax({
-           url: "/nodes",
-           dataType: 'json',
-           success : this.loadData,
-           error: function(){ console.log("AJAX request error: '/nodes'"); },
-           statusCode: {
-             200: function(){ console.log("304: Not modified"); },
-             304: function(){ console.log("200: Modified"); }
-           }
-        });
-     }
-
-     this.loadData = function(elements) {
-         console.log(elements);
-         window.elements = elements;
-         cy.load(elements);
-     }
-  }
-}); // on dom ready
