1c62ae1abbcb101ace2a5cd8b282c993706aa7bc
tadzjibov <k.z.tadzjibov@student.tudelft.nl>
2016-04-28 08:11:53 +0200

    D3 implementation loading 8mb data

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -15,7 +15,7 @@ $(function(){ // on dom ready
     for(var i = 0; i < data.nodes.length; i++) {
         var tempNode = data.nodes[i];
         var nodeWeight = tempNode.bubble? 100 : 50;
-        nodes.push({ data: { id: tempNode.id, name: getName(tempNode.data), weight: nodeWeight, faveColor: '#6FB1FC', faveShape: 'ellipse' }, position: {x: tempNode.x, y: tempNode.y} });
+        nodes.push({ data: { id: tempNode.id, name: getName(tempNode.data), weight: nodeWeight, faveColor: '#6FB1FC', faveShape: 'ellipse' }, position: {x: tempNode.x*100, y: tempNode.y*100} });
     }
 
 	var edges = [];
@@ -107,43 +107,43 @@ $(function(){ // on dom ready
         boxSelectionEnabled: false,
         autounselectify: true,
 
-        style: [{"selector":"core",
-                   "style":
-                   {"selection-box-color":"#AAD8FF","selection-box-border-color":"#8BB0D0","selection-box-opacity":"0.5"}},
-                {"selector":"node",
-                   "style":
-                    {"width":"mapData(score, 0, 0.006769776522008331, 20, 60)",
-                     "height":"mapData(score, 0, 0.006769776522008331, 20, 60)",
-                     "content":"data(name)","font-size":"12px","text-valign":"center","text-halign":"center",
-                     "background-color":"#555","text-outline-color":"#555","text-outline-width":"2px","color":"#fff",
-                     "overlay-padding":"6px","z-index":"10"}},
-                {"selector":"node[?attr]","style":{"shape":"rectangle","background-color":"#aaa","text-outline-color":"#aaa",
-                    "width":"16px","height":"16px","font-size":"6px","z-index":"1"}},
-                {"selector":"node[?query]","style":{"background-clip":"none","background-fit":"contain"}},
-                {"selector":"node:selected","style":{"border-width":"6px","border-color":"#AAD8FF","border-opacity":"0.5",
-                    "background-color":"#77828C","text-outline-color":"#77828C"}},
-                {"selector":"edge","style":{"curve-style":"haystack","haystack-radius":"0.5","opacity":"0.8",
-                    "line-color":"#bbb","width":"mapData(weight, 0, 1, 1, 8)","overlay-padding":"3px"}},
-                {"selector":"node.unhighlighted","style":{"opacity":"0.2"}},
-                {"selector":"edge.unhighlighted","style":{"opacity":"0.05"}},
-                {"selector":".highlighted","style":{"z-index":"999999"}},
-                {"selector":"node.highlighted","style":{"border-width":"6px","border-color":"#AAD8FF","border-opacity":"0.5",
-                    "background-color":"#394855","text-outline-color":"#394855",
-                    "shadow-blur":"12px","shadow-color":"#000","shadow-opacity":"0.8",
-                    "shadow-offset-x":"0px","shadow-offset-y":"4px"}},
-                {"selector": "node", "style": {"color": "#ECF0F1"}},
-                {"selector":"edge.filtered","style":{"opacity":"0"}},
-                {"selector":"edge[group=\"coexp\"]","style":{"line-color":"#d0b7d5"}},
-                {"selector":"edge[group=\"coloc\"]","style":{"line-color":"#a0b3dc"}},
-                {"selector":"edge[group=\"gi\"]","style":{"line-color":"#90e190"}},
-                {"selector":"edge[group=\"path\"]","style":{"line-color":"#9bd8de"}},
-                {"selector":"edge[group=\"pi\"]","style":{"line-color":"#eaa2a2"}},
-                {"selector":"edge[group=\"predict\"]","style":{"line-color":"#f6c384"}},
-                {"selector":"edge[group=\"spd\"]","style":{"line-color":"#dad4a2"}},
-                {"selector":"edge[group=\"spd_attr\"]","style":{"line-color":"#D0D0D0"}},
-                {"selector":"edge[group=\"reg\"]","style":{"line-color":"#D0D0D0"}},
-                {"selector":"edge[group=\"reg_attr\"]","style":{"line-color":"#D0D0D0"}},
-                {"selector":"edge[group=\"user\"]","style":{"line-color":"#f0ec86"}}],
+//        style: [{"selector":"core",
+//                   "style":
+//                   {"selection-box-color":"#AAD8FF","selection-box-border-color":"#8BB0D0","selection-box-opacity":"0.5"}},
+//                {"selector":"node",
+//                   "style":
+//                    {"width":"60",
+//                     "height":"60",
+//                     "content":"data(name)","font-size":"12px","text-valign":"center","text-halign":"center",
+//                     "background-color":"#555","text-outline-color":"#555","text-outline-width":"2px","color":"#fff",
+//                     "overlay-padding":"6px","z-index":"10"}},
+//                {"selector":"node[?attr]","style":{"shape":"rectangle","background-color":"#aaa","text-outline-color":"#aaa",
+//                    "width":"16px","height":"16px","font-size":"6px","z-index":"1"}},
+//                {"selector":"node[?query]","style":{"background-clip":"none","background-fit":"contain"}},
+//                {"selector":"node:selected","style":{"border-width":"6px","border-color":"#AAD8FF","border-opacity":"0.5",
+//                    "background-color":"#77828C","text-outline-color":"#77828C"}},
+//                {"selector":"edge","style":{"curve-style":"haystack","haystack-radius":"0.5","opacity":"0.8",
+//                    "line-color":"#bbb","width":"mapData(weight, 0, 1, 1, 8)","overlay-padding":"3px"}},
+//                {"selector":"node.unhighlighted","style":{"opacity":"0.2"}},
+//                {"selector":"edge.unhighlighted","style":{"opacity":"0.05"}},
+//                {"selector":".highlighted","style":{"z-index":"999999"}},
+//                {"selector":"node.highlighted","style":{"border-width":"6px","border-color":"#AAD8FF","border-opacity":"0.5",
+//                    "background-color":"#394855","text-outline-color":"#394855",
+//                    "shadow-blur":"12px","shadow-color":"#000","shadow-opacity":"0.8",
+//                    "shadow-offset-x":"0px","shadow-offset-y":"4px"}},
+//                {"selector": "node", "style": {"color": "#ECF0F1"}},
+//                {"selector":"edge.filtered","style":{"opacity":"0"}},
+//                {"selector":"edge[group=\"coexp\"]","style":{"line-color":"#d0b7d5"}},
+//                {"selector":"edge[group=\"coloc\"]","style":{"line-color":"#a0b3dc"}},
+//                {"selector":"edge[group=\"gi\"]","style":{"line-color":"#90e190"}},
+//                {"selector":"edge[group=\"path\"]","style":{"line-color":"#9bd8de"}},
+//                {"selector":"edge[group=\"pi\"]","style":{"line-color":"#eaa2a2"}},
+//                {"selector":"edge[group=\"predict\"]","style":{"line-color":"#f6c384"}},
+//                {"selector":"edge[group=\"spd\"]","style":{"line-color":"#dad4a2"}},
+//                {"selector":"edge[group=\"spd_attr\"]","style":{"line-color":"#D0D0D0"}},
+//                {"selector":"edge[group=\"reg\"]","style":{"line-color":"#D0D0D0"}},
+//                {"selector":"edge[group=\"reg_attr\"]","style":{"line-color":"#D0D0D0"}},
+//                {"selector":"edge[group=\"user\"]","style":{"line-color":"#f0ec86"}}],
 
 //        elements: {
 //          nodes: [
@@ -164,48 +164,22 @@ $(function(){ // on dom ready
 //          ]
 //        },
 
+        hideEdgesOnViewport : true,
+        hideLabelsOnViewport : true,
+        textureOnViewport : true,
+
+
         layout: {
           name: 'grid',
           padding: 10
         }
       });
-
-      var testJson = {
-                       "status": "success",
-                       "nodes": [
-                         {
-                           "id": 1,
-                           "bubble": false,
-                           "data": "ACGGT",
-                           "x": 0,
-                           "y": 0
-                         },
-                         {
-                           "id": 2,
-                           "bubble": true,
-                           "data": "bubble",
-                           "x": 100,
-                           "y": 100
-                         },
-                         {
-                           "id": 3,
-                           "bubble": false,
-                           "data": "GCCAGT",
-                           "x": -100,
-                           "y": -100
-                         }
-                       ],
-                       "edges": [
-                         {
-                           "from": 1,
-                           "to": 2
-                         },
-                         {
-                           "from": 2,
-                           "to": 3
-                         }
-                       ]
-                     };
+    var request = new XMLHttpRequest();
+       request.open("GET", "js/graph.json", false);
+       request.send(null)
+       var testJson = JSON.parse(request.responseText);
+//      var testJson = {"status":"success","nodes":[{"id":1,"bubble":false,"data":"TTGACCGATGACCCCGGTTCAGGCTTCACCACAGTGTGGAACGCGGTCGTCTCCGAACTTAACGGCGACCCTAAGGTTGACGACGGACCCAGCAGTGATGCTAATCTCAGCGCTCCGCTGACCCCTCAGCAAAGGGCTTGGCTCAATCTCGTCCAGCCATTGACCATCGTCGAGGGGTTTGCTCTGTTATCCGTGCCGAGCAGCTTTGTCCAAAACGAAATCGAGCGCCATCTGCGGGCCCCGATTACCGACGCTCTCAGCCGCCGACTCGGACATCAGATCCAACTCGGGGTCCGCATCGCTCCGCCGGCGACCGACGAAGCCGACGACACTACCGTGCCGCCTTCCGAAAATCCTGCTACCACATCGCC","x":0,"y":0},{"id":2,"bubble":false,"data":"AGACACCACAACCGACAACGACGAGATTGATGACAGCGCTGCGGCACGGGGCGATAACCAGCACAGTTGGCCAAGTTACTTCACCGAGCGCCCGCACAATACCGATTCCGCTACCGCTGGCGTAACCAGCCTTAACCGTCGCTACACCTTTGATACGTTCGTTATCGGCGCCTCCAACCGGTTCGCGCACGCCGCCGCCTTGGCGATCGCAGAAGCACCCGCCCGCGCTTACAACCCCCTGTTCATCTGGGGCGAGTCCGGTCTCGGCAAGACACACCTGCTACACGCGGCAGGCAACTATGCCCAACGGTTGTTCCCGGGAATGCGGGTCAAATATGTCTCCACCGAGGAATTCACCAACGACTTCATTAACTCGCTCCGCGATGACCGCAAGGTCGCATTCAAACGCAGCTACCGCGACGTAGACGTGCTGTTGGTCGACGACATCCAATTCATTGAAGGCAAAGAGGGTATTCAAGAGGAGTTCTTCCACACCTTCAACACCTTGCACAATGCCAACAAGCAAATCGTCATCTCATCTGACCGCCCACCCAAGCAGCTCGCCACCCTCGAGGACCGGCTGAGAACCCGCTTTGAGTGGGGGCTGATCACTGACGTACAACCACCCGAGCTGGAGACCCGCATCGCCATCTTGCGCAAGAAAGCACAGATGGAACGGCTCGCGGTCCCCGACGATGTCCTCGAACTCATCGCCAGCAGTATCGAACGCAATATCCGTGAACTCGAGGGCGCGCTGATCCGGGTCACCGCGTTCGCCTCATTGAACAAAACACCAATCGACAAAGCGCTGGCCGAGATTGTGCTTCGCGATCTGATCGCCGACGCCAACACCATGCAAATCAGCGCGGCGACGATCATGGCTGCCACCGCCGAATACTTCGACACTACCGTCGAAGAGCTTCGCGGGCCCGGCAAGACCCGAGCACTGGCCCAGTCACGACAGATTGCGATGTACCTGTGTCGTGAGCTCACCGATCTTTCGTTGCCCAAAATCGGCCAAGCGTTCGGCCGTGATCACACAACCGTCATGTACGCCCAACGCAAGATCCTGTCCGAGAT","x":10,"y":0},{"id":3,"bubble":false,"data":"C","x":20,"y":5},{"id":4,"bubble":false,"data":"G","x":20,"y":-5},{"id":5,"bubble":false,"data":"GCCGAGCGCCGTGAGGTCTTTGATCACGTCAAAGAACTCACCACTCGCATCCGTCAGCGCTCCAAGCGCTAGCACGGCGTGTTCTTCCGACAACGTTCTT","x":30,"y":0},{"id":6,"bubble":false,"data":"A","x":40,"y":0},{"id":7,"bubble":false,"data":"AAAAAACTTCTCTCTCCCAGGTCACACCAGTCACAGAGATTGGCTGTGAGTGTCGCTGTGCACAAACCGCGCACAGACTCATACAGTCCCGGCGGTTCCGTTCACAACCCACGCCTCATCCCCACCGACCCAACACACACCCCACAG","x":50,"y":0}],"edges":[{"from":1,"to":2},{"from":2,"to":3},{"from":2,"to":4},{"from":3,"to":5},{"from":4,"to":5},{"from":5,"to":6},{"from":5,"to":7},{"from":6,"to":7}]};
+      console.log(testJson)
       cy.add(JSONAdapter.prototype.convert(testJson));
 
       this.bindUIEvents();
