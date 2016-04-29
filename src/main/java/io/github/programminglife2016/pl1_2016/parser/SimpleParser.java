e6a1d66bd525d835d36426c72c67e38c6403abe4
Ravi Autar <ravi_autar27@hotmail.com>
2016-04-29 12:10:25 +0200

    Fixed checkstyle errors

--- src/main/java/io/github/programminglife2016/pl1_2016/parser/SimpleParser.java
+++ src/main/java/io/github/programminglife2016/pl1_2016/parser/SimpleParser.java
@@ -3,7 +3,11 @@ package io.github.programminglife2016.pl1_2016.parser;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * Temporary simple parser for parsing .gfa files.
