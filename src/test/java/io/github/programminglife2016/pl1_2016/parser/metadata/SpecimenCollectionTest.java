d83627c61b7a1ec5bafd53290e49f61eb296a531
Ravi Autar <ravi_autar27@hotmail.com>
2016-05-13 01:59:46 +0200

    Completed tests for specimencollection

--- src/test/java/io/github/programminglife2016/pl1_2016/parser/metadata/SpecimenCollectionTest.java
+++ src/test/java/io/github/programminglife2016/pl1_2016/parser/metadata/SpecimenCollectionTest.java
@@ -5,9 +5,12 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.util.Iterator;
+
+import static com.sun.org.apache.xml.internal.security.keys.keyresolver.KeyResolver.iterator;
 import static org.junit.Assert.assertEquals;
 
-/**
+/** Abstract test class for classes extending SpecimenCollection.
  * Created by ravishivam on 12-5-16.
  */
 @RunWith(MockitoJUnitRunner.class)
@@ -17,14 +20,32 @@ public abstract class SpecimenCollectionTest {
     @Mock
     private Specimen specimen;
 
+    /**
+     * Initialize local collection with the one that is passed on by extended
+     * class.
+     * @param collection Collection passed on by extended class.
+     */
     public void setSpecimenCollection(SpecimenCollection collection) {
         this.collection = collection;
     }
 
+    /**
+     * Test basic put and get method of the specimenCollection.
+     */
     @Test
     public void setFirstElement() {
         collection.put("specimen1", specimen);
         assertEquals(specimen, collection.get("specimen1"));
     }
 
+    /**
+     * Test the iterator of specimentCollection.
+     */
+    @Test
+    public void testContainsItem() {
+        collection.put("specimen1", specimen);
+        collection.put("specimen1", specimen);
+        Iterator<Subject> it = collection.iterator();
+        assertEquals(false, collection.containsKey((Specimen)it.next()));
+    }
 }
