034ab16d69c250c142227a4d8414fd0d1f5a1b43
Ravi Autar <ravi_autar27@hotmail.com>
2016-05-13 02:22:27 +0200

    Added new package for phyloparser to maintain structure

--- src/main/java/io/github/programminglife2016/pl1_2016/parser/phylotree/PhyloGeneticTreeParser.java
+++ src/main/java/io/github/programminglife2016/pl1_2016/parser/phylotree/PhyloGeneticTreeParser.java
@@ -1,6 +1,5 @@
 package io.github.programminglife2016.pl1_2016.parser.phylotree;
 
-import io.github.programminglife2016.pl1_2016.parser.JsonSerializable;
 import io.github.programminglife2016.pl1_2016.parser.Parser;
 import io.github.programminglife2016.pl1_2016.parser.TreeNodeCollection;
 import io.github.programminglife2016.pl1_2016.parser.TreeNodeList;
--- src/test/java/io/github/programminglife2016/pl1_2016/parser/phylotree/PhyloGeneticTreeParserTest.java
+++ src/test/java/io/github/programminglife2016/pl1_2016/parser/phylotree/PhyloGeneticTreeParserTest.java
@@ -1,5 +1,6 @@
-package io.github.programminglife2016.pl1_2016.parser;
+package io.github.programminglife2016.pl1_2016.parser.phylotree;
 
+import io.github.programminglife2016.pl1_2016.parser.TreeNodeCollection;
 import io.github.programminglife2016.pl1_2016.parser.phylotree.PhyloGeneticTreeParser;
 import org.junit.Before;
 import org.junit.Test;
