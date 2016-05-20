7b4fd2c4c4c6a6a4a0fb9a713987e493c492bf53
Ravi Autar <ravi_autar27@hotmail.com>
2016-05-20 20:22:03 +0200

    Completed parser for graphviz data

--- src/main/java/io/github/programminglife2016/pl1_2016/parser/nodes/GraphvizParser.java
+++ src/main/java/io/github/programminglife2016/pl1_2016/parser/nodes/GraphvizParser.java
@@ -1,6 +1,5 @@
 package io.github.programminglife2016.pl1_2016.parser.nodes;
 
-import io.github.programminglife2016.pl1_2016.collapser.GraphExplorer;
 import io.github.programminglife2016.pl1_2016.parser.JsonSerializable;
 import io.github.programminglife2016.pl1_2016.parser.Parser;
 
@@ -8,9 +7,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-
-import static javax.imageio.ImageIO.read;
-
 
 /**
  * Created by ravishivam on 20-5-16.
@@ -32,9 +28,10 @@ public class GraphvizParser implements Parser {
         BufferedReader reader = null;
         try {
             reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
+            reader.readLine();
             String line;
             while ((line = reader.readLine()) != null) {
-//                parseLine(line);
+                parseLine(line);
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -47,6 +44,33 @@ public class GraphvizParser implements Parser {
                 }
             }
         }
+    }
+
+    private void parseLine(String line) {
+        String[] data = line.split(" ");
+        switch (data[0]) {
+            case "node" :
+                handleNode(data);
+                break;
+            case "edge" :
+                handleEdge(data);
+                break;
+            case "stop" :
+                break;
+
+        }
+    }
+
+    private void handleNode(String[] data) {
+        Node node = new Segment(Integer.parseInt(data[1]));
+        node.setXY(Integer.parseInt(data[2]), Integer.parseInt(data[3]));
+        this.nodeCollection.put(node.getId(), node);
+    }
+
+    private void handleEdge(String[] data) {
+        int from = Integer.parseInt(data[1]);
+        int to = Integer.parseInt(data[2]);
+        this.nodeCollection.get(from).addLink(this.nodeCollection.get(to));
     }
 
 }
