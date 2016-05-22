8d334c9fabc1bcc9437a71c3ae626cb0a845f004
Ravi Autar <ravi_autar27@hotmail.com>
2016-05-22 15:54:12 +0200

    Added fix and positions for bubbles

--- src/main/java/io/github/programminglife2016/pl1_2016/collapser/BubbleMain.java
+++ src/main/java/io/github/programminglife2016/pl1_2016/collapser/BubbleMain.java
@@ -1,5 +1,6 @@
 package io.github.programminglife2016.pl1_2016.collapser;
 
+import io.github.programminglife2016.pl1_2016.parser.nodes.Node;
 import io.github.programminglife2016.pl1_2016.parser.nodes.NodeCollection;
 import io.github.programminglife2016.pl1_2016.parser.nodes.GraphvizParser;
 import io.github.programminglife2016.pl1_2016.parser.nodes.SegmentParser;
@@ -8,18 +9,28 @@ import io.github.programminglife2016.pl1_2016.server.api.RestServer;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Map;
 
 
 public class BubbleMain {
     private static final double NANOSECONDS_PER_SECOND = 1000000000;
 
     public static void main(String[] args) throws IOException {
+//        InputStream segis = BubbleMain.class.getClass().getResourceAsStream("/genomes/tb10_interestingpart.gfa");
+//        InputStream dotis = BubbleMain.class.getClass().getResourceAsStream("/genomes/tb10_interestingpart.dot");
         InputStream segis = BubbleMain.class.getClass().getResourceAsStream("/genomes/output.gfa");
         InputStream dotis = BubbleMain.class.getClass().getResourceAsStream("/genomes/output.txt");
         NodeCollection nodeCollection = (new GraphvizParser((new SegmentParser()).parse(segis))).parse(dotis);
 
         BubbleDispatcher dispatcher = new BubbleDispatcher(nodeCollection);
         nodeCollection = dispatcher.getLevelBubbles(0,4);
+        for(Map.Entry<Integer, Node> entry : nodeCollection.entrySet()) {
+            Node node = entry.getValue();
+            int x = (node.getStartNode().getX() + node.getEndNode().getX())/2;
+            int y = (node.getStartNode().getY() + node.getEndNode().getY())/2;
+            nodeCollection.get(entry.getKey()).setXY(x, y);
+        }
+
         System.out.println(nodeCollection.size());
 //        Server server = new RestServer(dispatcher.getLevelBubbles(0,7));
         Server server = new RestServer(nodeCollection);
