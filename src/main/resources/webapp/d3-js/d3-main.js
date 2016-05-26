0676f70849d41330cdabfcfcc076d22d42c14b22
Ali Smesseim <smesseim.ali@gmail.com>
2016-05-27 01:52:05 +0200

    Add minimap

--- src/main/resources/webapp/d3-js/d3-main.js
+++ src/main/resources/webapp/d3-js/d3-main.js
@@ -1,11 +1,16 @@
 var width = window.innerWidth - 10;
-var height = window.innerHeight - 10;
+var height = window.innerHeight - 300;
+var miniWidth = width;
+var miniHeight = 250;
 var maxZoomLevel = 100;
 
 var nodes;
 var edges;
 var x;
 var y;
+
+var miniX;
+var miniY;
 
 $.getJSON("/api/nodes", function (response) {
     nodes = response.nodes;
@@ -17,12 +22,22 @@ $.getJSON("/api/nodes", function (response) {
     y = d3.scale.linear()
         .domain([0, max(nodes, "y")])
         .range([height, 0]);
+
+    miniX = d3.scale.linear()
+        .domain([0, max(nodes, "x")])
+        .range([0, miniWidth]);
+    miniY = d3.scale.linear()
+        .domain([0, max(nodes, "y")])
+        .range([miniHeight, 0]);
     drawGraph();
+    drawMinimap();
 });
 
 var circle;
 var line;
 var zm;
+
+var minimap;
 
 function drawGraph() {
     zm = d3.behavior.zoom().x(x).y(y).scaleExtent([1, maxZoomLevel]).on("zoom", zoom);
@@ -66,7 +81,43 @@ function drawGraph() {
         .on("mouseout", tip.hide);
 }
 
+var rect;
+
+function drawMinimap() {
+    minimap = d3.select("body").append("svg")
+        .attr("width", miniWidth)
+        .attr("height", miniHeight)
+        .append("g");
+
+    minimap.selectAll("line")
+        .data(edges)
+        .enter()
+        .append("line")
+        .attr("x1", function (d) {return miniX(d.x1)})
+        .attr("y1", function (d) {return miniY(d.y1)})
+        .attr("x2", function (d) {return miniX(d.x2)})
+        .attr("y2", function (d) {return miniY(d.y2)})
+        .attr("stroke", function (d) {var x = d.gens * 2; return "rgb(" + (255- x) + "," + (127 - x) + "," + (0) + ")"})
+        .attr("stroke-width", function (d) {return Math.max(1, d.gens / 10)});
+
+    rect = minimap
+        .append("rect")
+        .attr("class", "minimapRect")
+        .attr("x", 0)
+        .attr("y", 0)
+        .attr("width", miniWidth)
+        .attr("height", miniHeight);
+}
+
 function zoom() {
+    var visibleX1 = miniX(x.invert(0));
+    var visibleX2 = miniX(x.invert(width));
+    var visibleY1 = miniY(y.invert(0));
+    var visibleY2 = miniY(y.invert(height));
+    rect.attr("x", visibleX1)
+        .attr("y", visibleY1)
+        .attr("width", visibleX2 - visibleX1)
+        .attr("height", visibleY2 - visibleY1);
     line.attr("x1", function (d) {return x(d.x1)})
         .attr("y1", function (d) {return y(d.y1)})
         .attr("x2", function (d) {return x(d.x2)})
