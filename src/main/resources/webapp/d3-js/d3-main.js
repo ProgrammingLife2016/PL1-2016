335bcbf90863969e44d3135a96290599a35a5199
Ali Smesseim <smesseim.ali@gmail.com>
2016-05-26 15:47:06 +0200

    Show nodes when zoomed in

--- src/main/resources/webapp/d3-js/d3-main.js
+++ src/main/resources/webapp/d3-js/d3-main.js
@@ -1,5 +1,6 @@
 var width = window.innerWidth - 10;
 var height = window.innerHeight - 10;
+var maxZoomLevel = 100;
 
 var nodes;
 var edges;
@@ -21,18 +22,27 @@ $.getJSON("/api/nodes", function (response) {
 
 var circle;
 var line;
+var zm;
 
 function drawGraph() {
+    zm = d3.behavior.zoom().x(x).y(y).scaleExtent([1, maxZoomLevel]).on("zoom", zoom);
     var svg = d3.select("body").append("svg")
         .attr("width", width)
         .attr("height", height)
         .append("g")
-        .call(d3.behavior.zoom().x(x).y(y).scaleExtent([1, 100]).on("zoom", zoom));
+        .call(zm);
 
     svg.append("rect")
         .attr("class", "overlay")
         .attr("width", width)
         .attr("height", height);
+
+    circle = svg.selectAll("circle")
+        .data(nodes)
+        .enter()
+        .append("circle")
+        .attr("r", 2.5)
+        .attr("transform", "translate(-9999, -9999)");
 
     line = svg.selectAll("line")
         .data(edges)
@@ -47,10 +57,16 @@ function drawGraph() {
 }
 
 function zoom() {
+    if (zm.scale() > 20) {
+        circle.attr("transform", transform);
+    } else {
+        circle.attr("transform", "translate(-9999, -9999)");
+    }
     line.attr("x1", function (d) {return x(d.x1)})
         .attr("y1", function (d) {return y(d.y1)})
         .attr("x2", function (d) {return x(d.x2)})
-        .attr("y2", function (d) {return y(d.y2)});
+        .attr("y2", function (d) {return y(d.y2)})
+        .attr("stroke-width", function (d) {return Math.max(1, d.gens / 10 / zm.scale())});
 }
 
 function transform(d) {
