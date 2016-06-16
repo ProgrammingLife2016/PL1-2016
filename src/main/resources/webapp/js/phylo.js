87cb79c0995c4d3384c28cf33c5450dcdd382218
aelkhalki <adamelkhalki@gmail.com>
2016-06-16 14:25:15 +0200

    Add querying in metadata and add faster api interaction

--- src/main/resources/webapp/js/phylo.js
+++ src/main/resources/webapp/js/phylo.js
@@ -117,14 +117,12 @@ function phylo(n, offset) {
       });
   }
 }
-
+window.genomes = [];
 function getMetadata(name) {
     console.log(name);
     var value = $.getJSON("/api/metadata/info/"+name, function (response) {
-        return response.subject[name];
-    }).responseText;
-    console.log(value);
-    return value;
+        window.genomes.push(response["subject"]);
+    });
 }
 d3.text("/static/file.nwk", function(text) {
   var x = newick.parse(text);
@@ -156,7 +154,6 @@ d3.text("/static/file.nwk", function(text) {
   treenode.append("circle")
       .attr("r", 2.5);
   window.tree = tree;
-
   var label = tree.selectAll("text")
       .data(treenodes.filter(function(d) { return d.x !== undefined && !d.children; }))
     .enter().append("text")
@@ -168,6 +165,7 @@ d3.text("/static/file.nwk", function(text) {
 //      .text(function(d) { return d.name; })
 //      .attr("metadata", function(d) { return getMetadata(d.name)})
       .attr("class", function(d) {
+            getMetadata(d.name);
             var str = $("."+d.name).attr("class");
             return str.replace("treelink", "");
             })
